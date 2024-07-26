a40ee9363e6725432726feb694da499a1509aa66
TheWitness <thewitness@cacti.net>
2024-07-26 19:23:00 -0400

    QA: Remove code not intended for multi-queries

--- functions.php
+++ functions.php
@@ -2946,7 +2946,6 @@ function run_flow_query($session, $query_id, $start, $end) {
 			$sql_inner1         = str_replace(array("\n", "\t"), array(' ', ''), $sql_inner1);
 			$sql_inner2         = str_replace(array("\n", "\t"), array(' ', ''), $sql_inner2);
 			$sql_query          = str_replace(array("\n", "\t"), array(' ', ''), $sql_query);
-			$sql_groupby        = str_replace(array("\n", "\t"), array(' ', ''), $sql_groupby);
 			$sql_inner_groupby1 = str_replace(array("\n", "\t"), array(' ', ''), $sql_inner_groupby1);
 			$sql_inner_groupby2 = str_replace(array("\n", "\t"), array(' ', ''), $sql_inner_groupby2);
 		}
