e8c45c493d401f065542610ac54139e4f59c3436
Petr Macek <petr.macek@kostax.cz>
2024-08-16 17:56:41 +0200

    at least one must be selected, fix default checkbox value (#209)
    * at least one must be selected, fix default checkbox value
    
    * newline

--- functions.php
+++ functions.php
@@ -502,6 +502,10 @@ function save_filter() {
 	$save['panel_packets']   = isset_request_var('panel_packets') ? 'on':'';
 	$save['panel_flows']     = isset_request_var('panel_flows') ? 'on':'';
 
+	if ($save['panel_table'] == '' && $save['panel_bytes'] == '' && $save['panel_packets'] == '' && $save['panel_flows'] == '') {
+		$save['panel_table'] = 'on';
+	}
+
 	$id = flowview_sql_save($save, 'plugin_flowview_queries', 'id', true);
 
 	if (is_error_message()) {
